ba7bfb6a834a3327662fcdb68bd57e252ad0a79c
devArist <aridev97@gmail.com>
2021-06-04 11:47:40 +0000

    backend side form verification

--- website/views.py
+++ website/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from . import models
 from service import models as service_models
+import re
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 def home(request):
@@ -18,5 +21,32 @@ def home(request):
     galleries = service_models.Gallery.objects.filter(status=True)
     return render(request, 'index.html', locals())
 
+@csrf_exempt
 def checkup(request):
-    pass
+    success = True
+    text = ''
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        name = request.POST.get('name')
+        message = request.POST.get('message')
+
+        if email.isspace() or email == '' or name.isspace() or name == '' or message.isspace() or message == '':
+            success = False
+            text = 'Veuillez remplir les champs vides'
+        elif not re.fullmatch('(\w\.?)+@(\w\.?)+\.[A-Za-z]{2,3}', email):
+            success = False
+            text = 'Veuillez saisir un email valide'
+        elif not re.fullmatch("[A-Za-z'éèëöüïäû ]+", name):
+            success = False
+            text = 'Veuillez saisir un nom valide'
+        else:
+            contact = models.Contact(name=name, message=message, email=email)
+            contact.save()
+            text = 'Votre message a bien été enregistré'
+
+    datas = {
+        'success':success,
+        'text':text
+    }
+
+    return JsonResponse(datas, safe=False)
